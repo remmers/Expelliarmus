ef9457f1bb20cc112ca10a39f008f0125a8be712
remmers <julian.remmers@student.uibk.ac.at>
2017-12-12 09:38:42 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from VMIManagement import VMIManager
+from ContainerManagement import AppContainerizationUbuntu
+
+#vmiManager = VMIManager.getVMIManager("VMIs/VMI_ug_fcegv.img")
+
+contManager = AppContainerizationUbuntu("firefox", forceNew=False)
+contManager.runApplication()
