14cd0ac75bab83e3f3c5a4234c15f08c0a81addd
remmers <julian.remmers@student.uibk.ac.at>
2018-02-13 14:43:42 +0100

    centralized static path information

---
+++ StaticInfo.py
@@ -0,0 +1,8 @@
+class StaticInfo:
+    relPathLocalRepository = "localRepository"
+    relPathLocalRepositoryPackages = relPathLocalRepository + "/packages"
+    relPathLocalRepositoryBaseImages = relPathLocalRepository + "/BaseImages"
+    relPathLocalRepositoryUserFolders = relPathLocalRepository + "/UserFolders"
+    relPathLocalRepositoryDatabase = relPathLocalRepository + "/db_repo_metadata.sqlite"
+    relPathGuestRepoConfigs = "VMIRepoConfigFiles"
+    relPathLocalVMIFolder = "VMIs"
